204279fac00cbcbc451191cf5f622d58c8467846
Bahattin <51765819+battincik@users.noreply.github.com>
2026-09-18 04:55:10 +0300

    client: remove NextAuth provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { SessionProvider } from 'next-auth/react';
 import { LanguageProvider } from '@/context/LanguageContext';
 import { AuthProvider } from '@/context/AuthContext';
 import { DatabaseProvider } from '@/context/DatabaseContext';
@@ -9,17 +8,15 @@ import { LegacyTranslationBridge } from '@/components/legacy-translation-bridge'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <LanguageProvider>
-        <AuthProvider>
-          <DatabaseProvider>
-            <LegacyTranslationBridge />
-            {children}
-            <LanguageSwitcher />
-          </DatabaseProvider>
-        </AuthProvider>
-      </LanguageProvider>
-    </SessionProvider>
+    <LanguageProvider>
+      <AuthProvider>
+        <DatabaseProvider>
+          <LegacyTranslationBridge />
+          {children}
+          <LanguageSwitcher />
+        </DatabaseProvider>
+      </AuthProvider>
+    </LanguageProvider>
   );
 }
 export default Providers;
