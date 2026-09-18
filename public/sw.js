778dd0b19f0e7f63e682b6f99622a9c0f58bf43a
Bahattin <51765819+battincik@users.noreply.github.com>
2026-09-18 22:52:43 +0300

    chore: remove unused browser asset public/sw.js

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', () => {
-    console.log('Service Worker installed.');
-});
-
-self.addEventListener('fetch', (event) => {
-    console.log('Fetching:', event.request.url);
-});
